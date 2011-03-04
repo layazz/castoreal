5cd9f44852e9f6c94e2cb6bef5bcced81933f161
Yazmin Ceniceros <yazmin.cf@gmail.com>
2011-03-03 20:31:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castorealo::Application.config.secret_token = '1aab8ecdf7ec57938b07a5d0290c52e0d02a2d04f71e2ca624ea21ccf592c6cc3b09752cbf4812884acb30a7946a25b48991cda5c4e09c4be6fba4b0124506f0'
